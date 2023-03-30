a598326151ccf3f86c53003433493ff7213aec7f
Ilya <felagunt@live.ru>
2023-03-30 15:15:26 +0300

    viewModel staff

--- app/src/main/java/com/example/chatapp/presentation/username/UsernameViewModel.kt
+++ app/src/main/java/com/example/chatapp/presentation/username/UsernameViewModel.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class UsernameViewModel @Inject constructor(
+class UsernameViewModel (
 
 ): ViewModel() {
     private val _usernameText = mutableStateOf("")
